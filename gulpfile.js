46d642316a53056cd3c3e0b94d364bf0a223a339
sexfat <bryant6831@gmail.com>
2023-05-03 10:08:14 +0800

    add

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,32 @@
-const {
-    src,
-    dest,
-    series,
-    parallel,
-    watch
-} = require('gulp');
+const { src, dest, series, parallel, watch } = require("gulp");
 
-
-function defaultTask(cb){
-   console.log(" gulp ok");
-   cb();
+function defaultTask(cb) {
+  console.log(" gulp ok");
+  cb();
 }
 
-exports.ok = defaultTask
+exports.ok = defaultTask;
 
+//任務A
+function TaskA(cb) {
+  console.log(" taskA");
+  cb();
+}
 
+// 任務 B
+function TaskB(cb) {
+  console.log("taskB");
+  cb();
+}
 
+exports.s = series(TaskA, TaskB); // 不同步執行
+exports.p = parallel(TaskA, TaskB); // 同步執行
+
+// =========================   src / dest ======================== //
+
+function sassstyle() {
+  return src("./sass/style.scss").pipe(dest("./dist/css"));
+}
+
+exports.style = sassstyle
 
