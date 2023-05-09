343af79237b53334cedce5a12104f0479525600f
sexfat <bryant6831@gmail.com>
2023-05-09 10:06:04 +0800

    add webpack

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,12 @@
+// jquery
+import $ from "jquery";
+
+
+$('body').css("background-color" , "blue");
+
+
+//
 const x = 10;
 let func = (x , y) => x * y;
-console.log(func(40 , 30));
+console.log(func(40 , 90));
 console.log('webpack watch')
