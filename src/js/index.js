5649f563dcb7c7ab36e33f8be23be4c500fa0c99
sexfat <bryant6831@gmail.com>
2023-05-09 11:09:46 +0800

    add

--- src/js/index.js
+++ src/js/index.js
@@ -3,17 +3,20 @@ import $ from "jquery";
 import gsap from "gsap";
 import './style.css';
 
-
+//jquery
 $('body').css("background-color" , "blue");
 
+
+// gsap
 gsap.to('.box', {
   x: 500,
   y: 400,
-  duration: 1
+  duration: 5,
+  backgroundColor: "green"
 });
 
 
-//
+// init
 const x = 10;
 let func = (x , y) => x * y;
 console.log(func(40 , 90));
